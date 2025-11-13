1d9707ae8e1bc2de6f27907bb56ec3f1996e724a
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2025-11-13 15:44:00 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,29 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    // lichen
+    "bg-lichen-100",
+    "bg-lichen-500",
+    "text-moss-500",
+    // sun
+    "bg-sun-100",
+    "bg-sun-500",
+    "text-sun-900",
+    // nectar
+    "bg-nectar-100",
+    "bg-nectar-300",
+    "text-nectar-900",
+    // iris
+    "bg-iris-100",
+    "bg-iris-500",
+    "text-iris-900",
+    // any borders you use
+    "border-moss-500",
+    "border-sun-900",
+    "border-nectar-900",
+    "border-iris-900",
+  ],
   theme: {
     extend: {
       colors: {
@@ -12,29 +35,38 @@ export default {
           "500": "#CDF3A3",
         },
         moss: {
-          "900": "#0C2725",
           "100": "#F2FBF8",
           "500": "#1F504A",
-          "900": "#000000"
-          
+          "900": "#0C2725",
         },
         sand: "#FBF9F6",
         
         nectar: {
+          "100": "#FDF5F3",
           "300": "#FFAA99",
           "500": "#EF7E5F",
+          "900": "#933A21"
         },
         moon: {
           "300": "#ECEDED",
         },
         iris: {
+          "100": "#F5F4FE",
           "500": "#9F93F2",
+          "900": "#4D2CAB"
         },
         sun: {
+          "100": "#FDFBE9",
           "500": "#F9EE8F",
+          "900": "#725F00"
         }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
 }
